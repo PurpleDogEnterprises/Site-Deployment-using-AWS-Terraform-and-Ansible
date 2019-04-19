3860ae294243b859143878e77fac4c8285273a72
Sean Guthrie (Purple Dog Enterprises) <seanpatrickguthrie@gmail.com>
2019-04-19 10:30:47 -0500

    Add files via upload

--- terransible/variables.tf
+++ terransible/variables.tf
@@ -13,3 +13,7 @@ variable "db_instance_class" {}
 variable "dbname" {}
 variable "dbuser" {}
 variable "dbpassword" {}
+variable "key_name" {}
+variable "public_key_path" {}
+variable "dev_instance_type" {}
+variable "dev_ami" {}
