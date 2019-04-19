54dd3dbc0a3ba3403b980419b740fbe50e3361ac
Sean Guthrie (Purple Dog Enterprises) <seanpatrickguthrie@gmail.com>
2019-04-18 19:12:20 -0500

    Update main.tf

--- terransible/main.tf
+++ terransible/main.tf
@@ -63,6 +63,7 @@ resource "aws_vpc" "wp_vpc" {
   }
 }
 
+
 #internet gateway
 
 resource "aws_internet_gateway" "wp_internet_gateway" {
@@ -72,6 +73,7 @@ resource "aws_internet_gateway" "wp_internet_gateway" {
     Name = "wp_igw"
   }
 }
+
 
 #route tables
 
@@ -173,6 +175,7 @@ resource "aws_subnet" "wp_rds3_subnet" {
   }
 }
 
+
 #subnet associations
 
 resource "aws_route_table_association" "wp_public_assoc" {
@@ -208,5 +211,113 @@ resource "aws_db_subnet_group" "wp_rds_subnetgroup" {
 
   tags {
     Name = "wp_rds_sng"
+  }
+}
+
+
+#Security groups
+
+resource "aws_security_group" "wp_dev_sg" {
+  name        = "wp_dev_sg"
+  description = "Used for access to the dev instance"
+  vpc_id      = "${aws_vpc.wp_vpc.id}"
+
+  #SSH
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${var.localip}"]
+  }
+
+  #HTTP
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["${var.localip}"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+#Public Security group
+
+resource "aws_security_group" "wp_public_sg" {
+  name        = "wp_public_sg"
+  description = "Used for public and private instances for load balancer access"
+  vpc_id      = "${aws_vpc.wp_vpc.id}"
+
+  #HTTP 
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  #Outbound internet access
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+#Private Security Group
+
+resource "aws_security_group" "wp_private_sg" {
+  name        = "wp_private_sg"
+  description = "Used for private instances"
+  vpc_id      = "${aws_vpc.wp_vpc.id}"
+
+  # Access from other security groups
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["${var.vpc_cidr}"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+#RDS Security Group
+
+resource "aws_security_group" "wp_rds_sg" {
+  name        = "wp_rds_sg"
+  description = "Used for DB instances"
+  vpc_id      = "${aws_vpc.wp_vpc.id}"
+
+  # SQL access from public/private security group
+
+  ingress {
+    from_port = 3306
+    to_port   = 3306
+    protocol  = "tcp"
+
+    security_groups = ["${aws_security_group.wp_dev_sg.id}",
+      "${aws_security_group.wp_public_sg.id}",
+      "${aws_security_group.wp_private_sg.id}",
+    ]
   }
 }
