055b7558f83385bc972b75f41e25f2b183c9a9ee
Sean Guthrie (Purple Dog Enterprises) <seanpatrickguthrie@gmail.com>
2019-04-21 07:17:38 -0500

    Add files via upload

--- terransible/variables.tf
+++ terransible/variables.tf
@@ -17,3 +17,13 @@ variable "key_name" {}
 variable "public_key_path" {}
 variable "dev_instance_type" {}
 variable "dev_ami" {}
+variable "elb_healthy_threshold" {}
+variable "elb_unhealthy_threshold" {}
+variable "elb_timeout" {}
+variable "elb_interval" {}
+variable "asg_max" {}
+variable "asg_min" {}
+variable "asg_grace" {}
+variable "asg_hct" {}
+variable "asg_cap" {}
+variable "lc_instance_type" {}
